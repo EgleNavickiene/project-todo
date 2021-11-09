76a76906f67a76f86cdf9be72a54b7d738136fac
EgleNavickiene <navickiene@gmail.com>
2021-11-09 21:00:41 +0200

    json server db file, app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from './interfaces/task';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-todo';
+
+  public tasks: Task[] = [];
+
+  constructor(private _taskService: TaskService) {
+    
+    this._taskService.getTasks()
+    .subscribe((data: Task[]) => {
+      this.tasks = data;
+    });
+  }
 }
